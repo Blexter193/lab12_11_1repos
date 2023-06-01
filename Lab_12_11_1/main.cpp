14cfb44dd0abf8f7db63adf938bf94699fbbc849
Blexter193 <oleksiy29032005@gmail.com>
2023-06-01 23:43:45 +0300

    Initial Commit

---
+++ Lab_12_11_1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab_12_11_1
+//
+//  Created by USER on 01.06.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
